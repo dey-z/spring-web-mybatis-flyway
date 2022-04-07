57745e31e4385227b806b2fe081f213a0b3f0168
dey-z <anish.13dey@gmail.com>
2022-04-07 23:33:32 +0900

    boundSql not working

--- src/main/java/co/jp/spring/usecasemib/mapper/master/ProjectMapper.java
+++ src/main/java/co/jp/spring/usecasemib/mapper/master/ProjectMapper.java
@@ -2,14 +2,36 @@ package co.jp.spring.usecasemib.mapper.master;
 
 import co.jp.spring.usecasemib.model.Project;
 import java.util.List;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-@Mapper
-public interface ProjectMapper {
-  @Select("select * from V_PJ_Foo")
-  List<Project> findAll();
+@Component
+public class ProjectMapper {
 
-  @Select("select * from V_PJ_Foo where ProjectId=#{projectId}")
-  Project findOne(String projectId);
+  private final SqlSession sqlSession;
+  private final SqlSessionFactory sqlSessionFactory;
+
+  public ProjectMapper(
+      @Qualifier("masterSqlSessionTemplate") SqlSession sqlSession,
+      @Qualifier("masterSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    this.sqlSession = sqlSession;
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  public List<Project> findAll() {
+//    BoundSql boundSql =
+//        this.sqlSessionFactory.getConfiguration().getMappedStatement("findAll").getBoundSql(null);
+//    System.out.println("Executed SQL" + System.lineSeparator() + boundSql.getSql());
+    return this.sqlSession.selectList("findAll");
+  }
+
+  public Project findOne(String projectId) {
+//    BoundSql boundSql =
+//        this.sqlSessionFactory.getConfiguration().getMappedStatement("findOne")
+//            .getBoundSql(projectId);
+//    System.out.println("Executed SQL" + System.lineSeparator() + boundSql.getSql());
+    return this.sqlSession.selectOne("findOne", projectId);
+  }
 }
