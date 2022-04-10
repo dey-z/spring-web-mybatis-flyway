f11463129671d87601caedda0e426573fff233b8
dey-z <anish.13dey@gmail.com>
2022-04-10 22:08:03 +0900

    seed data only when custom property is set

--- src/main/java/co/jp/spring/usecasemib/batch/HogeSeeder.java
+++ src/main/java/co/jp/spring/usecasemib/batch/HogeSeeder.java
@@ -2,18 +2,21 @@ package co.jp.spring.usecasemib.batch;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
 public class HogeSeeder {
+  @Value("${custom.property.seed.init}")
+  private Boolean seedInit;
+
   public static void main(String[] args) {
     // java -cp target/usecasemib-0.0.1-SNAPSHOT.jar -Dloader.main=co.jp.spring.usecasemib.batch.HogeSeeder org.springframework.boot.loader.PropertiesLauncher
     System.out.println("multi springboot applications for batch requirements");
@@ -25,15 +28,18 @@ public class HogeSeeder {
   }
 
   @Bean
-  @DependsOn({"flywayInitializer"})
   public DataSourceInitializer dataSourceInitializer(
       @Qualifier("primaryDataSource") HikariDataSource dataSource) {
-    ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-    resourceDatabasePopulator.addScript(new ClassPathResource("db/seed/hoge.sql"));
-    // resourceDatabasePopulator.setContinueOnError(true);
-    DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-    dataSourceInitializer.setDataSource(dataSource);
-    dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-    return dataSourceInitializer;
+    if (seedInit) {
+      ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+      resourceDatabasePopulator.addScript(new ClassPathResource("db/seed/hoge.sql"));
+      resourceDatabasePopulator.setContinueOnError(true);
+      DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+      dataSourceInitializer.setDataSource(dataSource);
+      dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+      return dataSourceInitializer;
+    } else {
+      return null;
+    }
   }
 }
